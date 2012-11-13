85c0eab6c61e359bc92885190d98afaff1c6dc39
Daniel Krykorka <m4w3r1ck@gmail.com>
2012-11-13 22:53:08 +0100

    bug fix
    Pravdepodobne dokoncena detekce aut

--- run.m
+++ run.m
@@ -10,11 +10,11 @@ end
 trafficObj = mmreader('../00012.avi'); %nactu video
 nframes = get(trafficObj, 'NumberOfFrames'); %pocet snimku ve videu
 duration = get(trafficObj, 'Duration'); % delka videa
-
-se = strel('square',10); % fill a gap
+H = fspecial('log',3,0.6);
+se = strel('square',5); % fill a gap
 width = 2; %velikost znacici kostky
 
-% rec = avifile('motion3.avi','Compression', 'FFDS', 'fps', get(trafficObj, 'FrameRate'  ));
+% rec = avifile('motion3.avi', 'Compression', 'i420', 'fps', get(trafficObj, 'FrameRate'  ));
 disp('getting bacground image...');
 try
     bcg= double(imread('bcg.bmp'));
@@ -29,36 +29,36 @@ trafficLane = GetTrafficLane(bcg,0);
 L = trafficLane.surfLeft(:,:,1) + trafficLane.surfLeft(:,:,2);
 R = trafficLane.surfRight(:,:,1) + trafficLane.surfRight(:,:,2);
 LR = L+R;
-fig = figure;
-set(fig,'Renderer','OpenGL')
-opengl verbose 
-%h = waitbar(0, 'processing');
+fig = figure(1);
+subplot(1,2,1);
+h = waitbar(0, 'processing');
 disp('counting cars...')
 for i=1:nframes
-    %waitbar(i/nframes, h);
+    tic
+    waitbar(i/nframes, h);
     I = double(read(trafficObj, i));
     %I = imfilter(I,G); % odstraneni sumu
-    
-    D = I./bcg; % rozdiln smiku od pozadi
-    M = D<1; % binarni maska rozdilnych pixelu
-    bcg = bcg + (0.2*(1-M)+0.02*M).*D; %aktualizovat pozadi
-    D = bgremove(I,bcg, 20);
-    %bw = bwmorph(D,'erode',2); %erode to remove small moise
+    I = imadd(imfilter(I,H),I); % posilit hrany
+    D = uint8(I./bcg); % rozdiln smiku od pozadi
+    M = uint8(D<0.9); % binarni maska rozdilnych pixelu
+    bcg = bcg + double((0.2*(1-M)+0.02*M).*D); %aktualizovat pozadi
+    D = bgremove(I,bcg, 30);
+%     bw = bwmorph(D,'erode',2); %erode to remove small moise
     bw = bwmorph(D,'close');
     %bw2 = imfilter(bw, P); %spojeni prumerovanim - pomale
     
     %bw = imopen(D, se);
     
-    bo = imopen(bw.*LR,se);
-    cc = bwconncomp(bo); % separovat prvni jizdni pruh
+   % bo = imopen(bw);
+    cc = bwconncomp(bw.*LR); % separovat prvni jizdni pruh
     L1 = regionprops(cc , {'Centroid', 'Area','BoundingBox'});
-    idx = [L1.Area] > 1000; %vyprat pouze plochy s velkou plochou
+    idx = [L1.Area] > 1500; %vyprat pouze plochy s velkou plochou
 
     subplot(1,2,1);
     imshow(uint8(I));
     title(sprintf('snimek c.%d', i));
     subplot(1,2,2);
-    imshow(bo,[]);
+    imshow(bw,[]);
     
     if ~isempty(L1)
         title(sprintf(' detekovanych objektu: %d', sum(idx)));
@@ -72,12 +72,13 @@ for i=1:nframes
         subplot(1,2,2);
         plot(centroids(idx,1), centroids(idx,2), 'b*');
         hold off
-        %rec = addframe(rec,  getframe(fig));
+        toc
     end
-    
+%     rec = addframe(rec,  getframe(fig));
+%     rec = addframe(rec, im2frame(uint8(bo), cmap));
     %rec = addframe(rec, uint8(ed));
 %     imshow(ed);
 end
 close(h)
-% close(rec)
-% clear(rec)
+% rec = close(rec);
+clear(rec)
