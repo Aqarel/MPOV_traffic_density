9c4b5d2d9b6de067cd03a8362649cb3b20b104d9
Daniel Krykorka <m4w3r1ck@gmail.com>
2012-11-30 20:20:46 +0100

    Kalman 3.
    priprava na snad filnalni zapis a pocitani

--- run.m
+++ run.m
@@ -27,6 +27,10 @@ end
 
 [MR,MC] = size(bcg);
 
+DOLNI_PRAH = 740;
+HORNI_PRAH = 110;
+COUNTED = 0;
+
 % Kalman filter initialization
 Rk=[[0.2845,0.0045]',[0.0045,0.0455]'];
 Hk=[[1,0]',[0,1]',[0,0]',[0,0]'];
@@ -41,14 +45,23 @@ x=zeros(1,4);
 xp_init = [MC/2,MR/2,0,0]';
 % strukt init
 
-s_init = struct('R', Rk, 'Q', Q, 'P',P, 'x', x, 'init', 0); % init strukt
+s_init = struct('R', Rk, 'Q', Q, 'P',P, 'x', x); % init struct
 cars = s_init; % structure array
+counted_cars = s_init;
 
 disp('separating traffic lines...')
 trafficLane = GetTrafficLane(bcg,0);
 L = trafficLane.surfLeft(:,:,1) + trafficLane.surfLeft(:,:,2);
 R = trafficLane.surfRight(:,:,1) + trafficLane.surfRight(:,:,2);
 LR = L+R;
+
+% generuje hranice, kde se vuz prestane sledovat
+Lp = L;
+Lp(1:DOLNI_PRAH,:)=0;
+Rp = R;
+Rp(HORNI_PRAH:end, :)=0;
+LRp = Lp+Rp;
+
 fig = figure(1);
 subplot(1,2,1);
 h = waitbar(0, 'processing');
@@ -59,19 +72,13 @@ for i=1:nframes
     tic
     waitbar(i/nframes, h);
     I = double(read(trafficObj, i));
-    %I = imfilter(I,G); % odstraneni sumu
     I = imadd(imfilter(I,H),I); % posilit hrany
     D = uint8(I./bcg); % rozdiln smiku od pozadi
     M = uint8(D<0.9); % binarni maska rozdilnych pixelu
     bcg = bcg + double((0.2*(1-M)+0.02*M).*D); %aktualizovat pozadi
     D = bgremove(I,bcg, 30);
-%     bw = bwmorph(D,'erode',2); %erode to remove small moise
-    bw = bwmorph(D,'close');
-    %bw2 = imfilter(bw, P); %spojeni prumerovanim - pomale
-    
-    %bw = imopen(D, se);
-    
-   % bo = imopen(bw);
+    bw = bwmorph(D,'close'); %erode to remove small moise
+
     cc = bwconncomp(bw.*LR); % separovat prvni jizdni pruh
     L1 = regionprops(cc , {'Centroid', 'Area','BoundingBox'});
     idx = [L1.Area] > 1500; %vyprat pouze plochy s velkou plochou
@@ -86,47 +93,78 @@ for i=1:nframes
         
         title(sprintf(' detekovanych objektu: %d', sum(idx)));
         centroids = cat(1,L1.Centroid);
-        boxes = cat(1,L1.BoundingBox);
+        centroids = round(centroids(idx, 1:2)); % delete noise
+        b = [];
+        for j=1:size(centroids,1)  % delete centroids in off area
+            if LRp(centroids(j,2),centroids(j,1)) == 1
+                b = [b j];
+            end
+        end
         hold on
+        co = centroids(b,:);
+        centroids(b,:) = [];
+        if size(co,1)>0
+            plot(co(1), co(2), 'ro'); % mark centroids - not counted
+        end 
+        
+        hold on
+        boxes = cat(1,L1.BoundingBox);
         subplot(1,2,1);
         for r=find(idx==1) % draw bounding boxes
             rectangle('Position',boxes(r,:));
         end
-        cc = centroids(idx,1);
-        cr = centroids(idx,2);
+        cc = centroids(:,1);
+        cr = centroids(:,2);
         subplot(1,2,2);
         plot(cc, cr, 'b*');
         
-        %kalman
-        if i ==1;
-            for j = 1:sum(idx) %init vector
-                cars(j) = s_init; 
-            end
-        end
-        
-        if size(cars,2)-sum(idx)< 0% add new detected car
+        %kalman - predikce polohy vozu
+
+        while size(cars,2)-sum(idx)-size(counted_cars,2)+1< 0% add new detected car
                 cars(size(cars,2)+1) = s_init;
         end
                 
-        for j = (size(cars,2) - sum(idx))+1:sum(idx)
-            if cars(j).init == 0
+        for j = 1:size(centroids,2)
+            if cars(j).x(1) == 0
                 xp = xp_init;
-                cars(j).init = 1;
             else
-                xp=A*cars(j).x(i-1,:)' + Bu;
+                xp=A*cars(j).x(end,:)' + Bu;
+                dx = cc - cars(j).x(end,1); % prirazeni spravneho bodu
+                dy = cr - cars(j).x(end,2);
+                d = dx.^2 + dy.^2;
+                ck = find(d == min(d));
             end
             PP = A*cars(j).P*A' + cars(j).Q;
             Kk = PP*Hk'*1/(Hk*PP*Hk'+Rk);
-            cars(j).x(i,:) = (xp + Kk*([cc(j),cr(j)]' - Hk*xp))';
+            if cars(j).x(1) == 0 % aby vektor x nezacinal nulama, #XXX: asi by slo napsat lip
+                cars(j).x(1,:) = (xp + Kk*([cc(1),cr(1)]' - Hk*xp))';
+                ck = 1; % jenom pro pripad, ze by to naslo najednou vic aut
+            else
+                cars(j).x(end+1,:) = (xp + Kk*([cc(ck),cr(ck)]' - Hk*xp))';
+            end
             cars(j).P = (eye(4)-Kk*Hk)*PP;
-            %end of klaman
+            
+            cc(ck) = []; % remove used elements
+            cr(ck) = [];
+            
+            a = round(cars(j).x(end,1:2));
+            
+            %zapocist, jestli bude v nasledujicim kroce za prahem
+            if LRp(a(2),a(1)) == 1 
+                 if counted_cars(size(counted_cars,1)).x(1) == 0
+                    counted_cars(1) = cars(j);
+                 else
+                   counted_cars(size(counted_cars,1)) = cars(j);
+                 end
+                 cars(j) = [];
+            end
         end
         
         
         toc
         
         %plot kalman prediction
-        for j = (size(cars,2) - sum(idx))+1:size(cars,2)
+        for j = 1:size(cars,2)
             plot(cars(j).x(:,1),cars(j).x(:,2), 'r-');
         end
         hold off
